61981132eca6cf8f69397b6a14076f166a632994
Patryk Wójtowicz <106282113+UltiPro@users.noreply.github.com>
2024-03-13 17:13:22 +0100

    Delete package jg.pb.rsi.java

--- Rozproszone systemy informatyczne/Pracownia Specjalistyczna nr 1/package jg.pb.rsi.java
+++
@@ -1,8 +0,0 @@
-package jg.pb.rsi.rmi;
-
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-
-public interface MyServerInt extends Remote {
-    String getDescription(String text) throws RemoteException;
-}
