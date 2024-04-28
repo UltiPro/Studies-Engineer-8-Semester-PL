147f15ef41a08e516366ccd314a0fe659d1f6ad2
Patryk Wójtowicz <ultiprogames@gmail.com>
2024-04-28 17:10:24 +0200

    Update MotorService.cs

--- Rozproszone systemy informatyczne/Projekt 1 Web Serwisy/SOAPMotor/MotorService.cs
+++ Rozproszone systemy informatyczne/Projekt 1 Web Serwisy/SOAPMotor/MotorService.cs
@@ -13,9 +13,75 @@ public class MotorService : IMotorService
         this._context = _context;
     }
 
-    public async Task<List<DBMotor>> GetAll()
+    public async Task Create(CreateMotor motor)
     {
-        return await _context.Motors.ToListAsync();
+        await _context.Motors.AddAsync(new DBMotor
+        {
+            Name = motor.Name,
+            RentPrice = motor.RentPrice
+        });
+
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task Remove(int id)
+    {
+        var motor = await _context.Motors.FirstOrDefaultAsync(motor => motor.Id == id);
+
+        if (motor is null) return;
+
+        _context.Motors.Remove(motor);
+
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task Update(UpdateMotor motor)
+    {
+        var motor2 = await _context.Motors.FirstOrDefaultAsync(m => m.Id == motor.Id);
+
+        if (motor2 is null) return;
+
+        motor2.Name = motor.Name;
+        motor2.RentPrice = motor.RentPrice;
+
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task<DetailMotor> Detail(int id)
+    {
+        var motor = await _context.Motors.FirstOrDefaultAsync(motor => motor.Id == id);
+
+        if (motor is null) return null;
+
+        return new DetailMotor
+        {
+            Id = motor.Id,
+            Name = motor.Name,
+            RentPrice = motor.RentPrice,
+            RentTo = motor.RentTo.ToString() ?? "",
+            Reservation = motor.Reservation ? "Reserved" : "Not reserved"
+        };
+    }
+
+    public async Task<List<DetailMotor>> GetAll()
+    {
+        var motors = await _context.Motors.ToListAsync();
+
+        var list = new List<DetailMotor>();
+
+        foreach (var motor in motors)
+        {
+            list.Add(new DetailMotor
+            {
+                Id = motor.Id,
+                Name = motor.Name,
+                RentPrice = motor.RentPrice,
+                RentTo = motor.RentTo.ToString() ?? "",
+                Reservation = motor.Reservation ? "Reserved" : "Not reserved"
+            });
+        }
+
+        return list;
     }
 
     public async Task<string> Reserve(int id)
@@ -28,7 +94,7 @@ public class MotorService : IMotorService
 
         motor.Reservation = true;
 
-        motor.Reservation = true;
+        await _context.SaveChangesAsync();
 
         return $"The motorbike has been reserved.";
     }
@@ -39,23 +105,29 @@ public class MotorService : IMotorService
 
         if (motor is null) return "Incorrect ID of motorbike.";
 
-        if (motor.Reservation) return "This motorbike is not reserved!";
+        if (!motor.Reservation) return "This motorbike is not reserved!";
 
         motor.Reservation = false;
+
+        await _context.SaveChangesAsync();
 
         return $"The motorbike is no longer reserved.";
     }
 
-    public async Task<string> Rent(int id, DateTime date)
+    public async Task<string> Rent(int id, int numberOfDays)
     {
         var motor = await _context.Motors.FirstOrDefaultAsync(motor => motor.Id == id);
 
         if (motor is null) return "Incorrect ID of motorbike.";
 
-        if (motor.RentTo != null && motor.RentTo > date) return $"Motorbike can not be rent before {date}.";
+        var rentDate = DateTime.Now.AddDays(numberOfDays);
 
-        motor.RentTo = date;
+        if (motor.RentTo != null && motor.RentTo < DateTime.Now) return $"Motorbike can not be rent before {motor.RentTo}.";
 
-        return "The motorbike has been rented.";
+        motor.RentTo = rentDate;
+
+        await _context.SaveChangesAsync();
+
+        return $"The motorbike has been rented to {rentDate}.";
     }
 }
