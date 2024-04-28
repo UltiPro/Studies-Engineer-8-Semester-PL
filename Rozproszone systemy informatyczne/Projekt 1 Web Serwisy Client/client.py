34633f855ec37c724819c79395816bd35ba0a6de
Patryk Wójtowicz <ultiprogames@gmail.com>
2024-04-28 17:15:10 +0200

    Create client.py

---
+++ Rozproszone systemy informatyczne/Projekt 1 Web Serwisy Client/client.py
@@ -0,0 +1,24 @@
+from zeep import Client
+
+client = Client(wsdl="http://localhost:5045/Motor.wsdl")
+
+# print(client.service.Create({"Name" : "test", "RentPrice": 1024}))
+# print(client.service.Remove(7))
+# print(client.service.Update({"Id": 5,"Name": "Jakiś motorek", "RentPrice": 2000}))
+# print(client.service.Detail(2))
+# print(client.service.GetAll())
+
+"""
+print(client.service.Reserve(3)) 
+print(client.service.Detail(3))
+"""
+
+"""
+print(client.service.CancelReserve(3)) 
+print(client.service.Detail(3))
+"""
+
+"""
+print(client.service.Rent(3, 4)) 
+print(client.service.Detail(3))
+"""
