2eda77242f3b930e19206463cf1a7f7c404c44fe
Patryk Wójtowicz <106282113+UltiPro@users.noreply.github.com>
2024-04-29 14:47:59 +0200

    RSI

---
+++ Rozproszone systemy informatyczne/Pracownia Specjalistyczna nr 8/WebApplication1/Controllers/HelloWorldController.cs
@@ -0,0 +1,37 @@
+﻿using System.Net;
+using System.Reflection;
+using System.Text;
+using System.Xml;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using WebApplication1.models;
+
+namespace WebApplication1.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class HelloWorldController : ControllerBase
+{
+    [HttpGet]
+    public string HelloWorld(string nick)
+    {
+        return $"Hello {nick}";
+    }
+
+    [HttpGet("/xml")]
+    [Produces("application/xml")]
+    public IActionResult GetUser()
+    {
+        var user = new User { Id = 1, Name = "Steve James" };
+        return Ok(user);
+    }
+
+    [HttpGet("/json")]
+    [Produces("application/json")]
+    public IActionResult GetUser2()
+    {
+        var user = new User { Id = 1, Name = "Steve James" };
+        return Ok(user);
+    }
+}
---
+++ Rozproszone systemy informatyczne/Pracownia Specjalistyczna nr 8/WebApplication1/models/User.cs
@@ -0,0 +1,7 @@
+﻿namespace WebApplication1.models;
+
+public class User
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+}
